0f32ca5984bfded259490ff01ad6d7193a076a3f
Jannis R <mail@jannisr.de>
2016-11-01 10:25:22 +0100

    adapt to older browsers
    I also removed unnecessary return statements.

--- index.js
+++ index.js
@@ -1,37 +1,33 @@
 /** @module google-maps-image-api */
 'use strict'
 
-const querystring = require( 'querystring' );
+var querystring = require( 'querystring' );
 
-const BASE_URL = 'https://maps.googleapis.com/maps/api/';
+var BASE_URL = 'https://maps.googleapis.com/maps/api/';
 
 module.exports = function( opts ) {
 
-	const zoom = typeof opts.zoom === 'number' ? opt.zoom : 14;
-	const size = opts.size || '320x240';
+	var zoom = typeof opts.zoom === 'number' ? opt.zoom : 14;
+	var size = opts.size || '320x240';
 
 	if( opts.type == 'staticmap' ) {
 
 		if( opts.center === undefined ) {
 
 			throw new Error( 'center must be defined in options' );
-			return;
 		} else if( opts.type === undefined ) {
 
 			throw new Error( 'type must be defined in options' );
-			return;
 		}
 	} else if( opts.type == 'streetview' ) {
 
 		if( opts.location === undefined && opts.pano === undefined ) {
 
 			throw new Error( 'you must pass in location in options' );
-			return;
 		}
 	} else {
 
 		throw new Error( opts.type + ' is an invalid type. Use "staticmap" or "streetview".' );
-		return;
 	}
 
 	return BASE_URL + opts.type + '?' + querystring.stringify( opts );
